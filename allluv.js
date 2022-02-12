2d9177a494245c0c4457206619d70b11b687edca
Alex <ah4605@stern.nyu.edu>
2022-02-11 21:07:00 -0500

    Basic functionality for popup button

--- allluv.js
+++ allluv.js
@@ -1,51 +1,51 @@
-document.body.style.border = "10px solid pink";
+// document.body.style.border = "10px solid pink";
 
 // Get stored settings
 chrome.storage.local.get(
-    {
-        music: "Marvin Gaye- Let's Get It On remix",
-        musicEnabled: "true",
-    },
-    function (data) {
-        if (data.musicEnabled) {
-            document.getElementById("stop-music").innerText = "Play";
-        } else {
-            document.getElementById("stop-music").innerText = "Stop";
-        }
+  {
+    music: "Marvin Gaye- Let's Get It On remix",
+    musicEnabled: "true",
+  },
+  function (data) {
+    if (data.musicEnabled) {
+      document.getElementById("stop-music").innerText = "Play";
+    } else {
+      document.getElementById("stop-music").innerText = "Stop";
     }
+  }
 );
 
 // Music on/off button
 document.getElementById("stop-music").addEventListener("click", function () {
-    chrome.storage.local.get(
-        {
-            musicEnabled: true,
-        },
-        function (data) {
-            console.log(data);
-            if (data.musicEnabled) {
-                // Turn off music
-                document.getElementById("stop-music").innerText = "Stop";
-                chrome.storage.local.set({
-                    musicEnabled: false,
-                });
-            } else {
-                // Turn on music
-                document.getElementById("stop-music").innerText = "Play";
-                chrome.storage.local.set({
-                    musicEnabled: true,
-                });
-            }
-        }
-    );
+  chrome.storage.local.get(
+    {
+      musicEnabled: true,
+    },
+    function (data) {
+      console.log(data);
+      if (data.musicEnabled) {
+        // Turn off music
+        document.getElementById("stop-music").innerText = "Stop";
+        chrome.storage.local.set({
+          musicEnabled: false,
+        });
+      } else {
+        // Turn on music
+        document.getElementById("stop-music").innerText = "Play";
+        chrome.storage.local.set({
+          musicEnabled: true,
+        });
+      }
+    }
+  );
 });
 
 // Pause music when page closes
 // This only works on the popup opened from the notification, not the browserAction button
 window.addEventListener(
-    "beforeunload",
-    function (e) {
-        chrome.runtime.sendMessage("pause");
-    },
-    false
+  "beforeunload",
+  function (e) {
+    chrome.runtime.sendMessage("pause");
+  },
+  false
 );
